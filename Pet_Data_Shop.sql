54cf3c50a8c045af3ccb5f67d3179609d29a2fbf
TuanThanh-dev19 <phanthanh19112005@gmail.com>
2026-01-09 14:02:09 +0700

    Update Service. Add column servicetype

--- Pet_Data_Shop.sql
+++ Pet_Data_Shop.sql
@@ -207,6 +207,11 @@ GO
 ========================================================= */
 CREATE TABLE dbo.Services (
     ServiceID       INT IDENTITY(1,1) PRIMARY KEY,
+
+    ServiceType     VARCHAR(20) NOT NULL
+        CONSTRAINT DF_Services_ServiceType DEFAULT 'hygiene'
+        CONSTRAINT CK_Services_ServiceType CHECK (ServiceType IN ('vaccination','boarding','hygiene')),
+
     Name            NVARCHAR(120) NOT NULL,
     Description     NVARCHAR(500) NULL,
     BasePrice       DECIMAL(12,2) NOT NULL CHECK (BasePrice >= 0),
