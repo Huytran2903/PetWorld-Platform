45704732dcb3793d1c176baab8d871c10e5a4b0a
Huy_Tran <Huytpn.ce190719@gmail.com>
2026-01-27 10:59:42 +0700

    add viewProfile v2

--- src/main/java/petworldplatform/controller/CustomerController.java
+++ src/main/java/petworldplatform/controller/CustomerController.java
@@ -1,4 +1,13 @@
 package petworldplatform.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CustomerController {
+
+    @GetMapping("/profile")
+    public String profileSetting() {
+        return "auth/viewProfile"; // viewProfile.html
+    }
 }
