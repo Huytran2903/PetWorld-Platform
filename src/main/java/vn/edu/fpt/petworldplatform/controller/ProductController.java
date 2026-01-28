4bbdeb3d3f48afe4dc67520f4723f585f08b2f7d
OanhTP <OanhTP.CE190857@gmail.com>
2026-01-28 09:45:17 +0700

    change css

--- src/main/java/vn/edu/fpt/petworldplatform/controller/ProductController.java
+++ src/main/java/vn/edu/fpt/petworldplatform/controller/ProductController.java
@@ -1,4 +1,30 @@
 package vn.edu.fpt.petworldplatform.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+
+    //Product List
+    @GetMapping("/products")
+    public String showProductList(Model model) {
+        model.addAttribute("formMode", "product");
+        return "product/product-list";
+    }
+
+    //Pet List
+    @GetMapping("/pets")
+    public String showPetList(Model model) {
+        model.addAttribute("formMode", "pet");
+        return "product/product-list";
+    }
+
+    //Product Detail
+    @GetMapping("/product/detail")
+    public String productDetail() {
+        return "product/product-detail";
+    }
+
 }
