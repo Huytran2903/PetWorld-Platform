b40eddd870a518e867ffe749b8bdc4bfe48d5437
Huy_Tran <Huytpn.ce190719@gmail.com>
2026-01-29 11:17:21 +0700

    add new (admin-dashboard, appt-detail, appt-manage, confirm staff for appointment), change name folder(" admin" to "admin")

--- src/main/java/vn/edu/fpt/petworldplatform/controller/AdminController.java
+++ src/main/java/vn/edu/fpt/petworldplatform/controller/AdminController.java
@@ -6,13 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-    @GetMapping("/staff/add")
+    @GetMapping("/admin/dashboard")
+    public String viewDashboard() {
+        return "admin/admin-dashboard";
+    }
+
+    @GetMapping("/admin/add-staff")
     public String addNewStaff() {
         return "admin/add-editStaffProfile";
     }
 
-    @GetMapping("/staff/edit")
+    @GetMapping("/admin/edit-staff")
     public String editStaffProfile() {
         return "admin/add-editStaffProfile";
+    }
+
+    @GetMapping("/admin/staff-manage")
+    public String showStaffList() {
+        return "admin/staff-manage";
+    }
+
+    @GetMapping("/admin/appointment-manage")
+    public String showAppointmentList() {
+        return "admin/appt-manage";
+    }
+
+    @GetMapping("/admin/appointment-manage/detail")
+    public String showAppointmentDetail() {
+        return " appointment/appt-detail";
     }
 }
