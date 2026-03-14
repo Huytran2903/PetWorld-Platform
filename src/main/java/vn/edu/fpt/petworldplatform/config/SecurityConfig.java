5ab136ce90467d5060dac296012c7d40fe678b82
TRANQUOCTHAI\quoct <quocthaitran26062005@gmail.com>
2026-03-14 23:34:42 +0700

    fix csrf security issue

--- src/main/java/vn/edu/fpt/petworldplatform/config/SecurityConfig.java
+++ src/main/java/vn/edu/fpt/petworldplatform/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(Customizer.withDefaults())
                 .securityContext(context -> context
                         .securityContextRepository(new HttpSessionSecurityContextRepository())
                 )
